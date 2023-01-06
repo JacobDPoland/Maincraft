2d8174640e3970a400ad26ea9c2c6aae533b16d9
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2023-01-06 12:51:33 -0600

    Completed disk storage tutorial video

--- src/org/mineacademy/server/MaincraftServer.java
+++ src/org/mineacademy/server/MaincraftServer.java
@@ -1,10 +1,14 @@
 package org.mineacademy.server;
 
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.mineacademy.server.entity.*;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class MaincraftServer {
@@ -20,16 +24,162 @@ public class MaincraftServer {
 //		server.testEntities();
 //		server.testSorting();
 //		server.testQueue();
-		server.testStorage();
+//		server.testTextStorage();
+//		server.testTextStorage2();
+//		server.testCsvStorage();
+		server.testYaml();
 	}
 
-	private void testStorage() {
-		File file = new File("names.txt");
+
+	private void testYaml() {
+
+		// create folder if it doesn't already exist
+		File folder = new File("files");
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+
+		// create file if it doesn't already exist
+		File file = new File(folder, "config.yml");
 		if (!file.exists()) {
 			try {
 				file.createNewFile();
 			} catch (IOException ex) {
-				System.err.println("Error creating file: " + file.getAbsolutePath());
+				System.err.println("Error creating config.yml");
+				ex.printStackTrace();
+			}
+		}
+
+		YamlConfiguration config = new YamlConfiguration();
+
+		try {
+			config.load(file);
+			config.set("Player", "Jaboobness");
+			config.save(file);
+			config.load(file);
+
+			String player = config.getString("Player");
+			System.out.println(player);
+
+		} catch (IOException | InvalidConfigurationException ex) {  // can also just use Throwable class
+			System.err.println("Error concerning config.yml");
+			ex.printStackTrace();
+		}
+
+
+	}
+
+
+	private void testCsvStorage() {
+		// creating dummy data to write
+		List<List<String>> rows = Arrays.asList(
+				Arrays.asList("Name", "Role", "Topic"),
+				Arrays.asList("Aidan", "CEO", "Money"),
+				Arrays.asList("Jacob", "CTO", "Java"),
+				Arrays.asList("Gort", "Mascot", "Food")
+		);
+
+		// create folder if it doesn't already exist
+		File folder = new File("files");
+		if (!folder.exists()) {
+			folder.mkdirs();
+		}
+
+		// create file if it doesn't already exist
+		File file = new File(folder, "data.csv");
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException ex) {
+				System.err.println("Error creating empty CSV file");
+				ex.printStackTrace();
+			}
+		}
+
+		// Write to csv
+		// Putting the FileWritter inside of the (parentheses) of the try ensures that Java closes and flushes
+		// the FileWriter automatically 
+		try (FileWriter write = new FileWriter(file)) {
+			for (List<String> row : rows) {
+				write.append(String.join(",", row));
+				write.append('\n');
+			}
+		} catch (IOException ex) {
+			System.err.println("Error writing data to CSV");
+			ex.printStackTrace();
+		}
+
+		// read from csv
+		try {
+			List<String> lines = Files.readAllLines(file.toPath());
+			for (String line : lines) {
+				// splitting here only replaces the comma with a space in the console output
+				String[] datas = line.split(",");
+				for (String data : datas) {
+					System.out.print(data + " ");
+				}
+				System.out.println("");  // newline character
+			}
+		} catch (IOException ex) {
+			System.err.println("Error reading from CSV file");
+			ex.printStackTrace();
+		}
+	}
+
+
+	private void testTextStorage2() {
+		{
+			String folderName = "files";
+			// MineAcademy uses this method for folder creation instead of Files.createdirectories()
+			File folder = new File(folderName);
+			if (!folder.exists()) {
+				folder.mkdirs();
+			}
+
+			// Create the file if it doesn't already exist
+			File file = new File(folder, "names.txt");
+			if (!file.exists()) {
+				try {
+					file.createNewFile();
+				} catch (IOException ex) {
+					System.err.println("Error creating folder or file: " + file.getAbsolutePath());
+					ex.printStackTrace();
+				}
+			}
+
+			// Write to file
+			List<String> lines = Arrays.asList("John", "Jacob", "Jingleheimershmit", "Gort");
+			try {
+				Files.write(file.toPath(), lines);
+			} catch (IOException ex) {
+				System.err.println("Error writing to file: " + file.getAbsolutePath());
+				ex.printStackTrace();
+			}
+			System.out.println("File written to " + file.getAbsolutePath());
+
+			// Read from file
+			List<String> readLines = new ArrayList<>();
+			try {
+				readLines.addAll(Files.readAllLines(file.toPath()));
+			} catch (IOException ex) {
+				System.err.println("Error reading from file at " + file.getAbsolutePath());
+				ex.printStackTrace();
+			}
+			System.out.println("File read from " + file.getAbsolutePath() + " is shown below:");
+			System.out.println(readLines);
+		}
+	}
+
+
+	private void testTextStorage() {
+		String folderName = "files";
+		File file = new File(folderName, "names.txt");
+		if (!file.exists()) {
+			try {
+				Files.createDirectories(Paths.get(folderName));  // MineAcademy does not use this
+				file.createNewFile();
+			} catch (IOException ex) {
+				System.err.println("Error creating folder or file: " + file.getAbsolutePath());
 				ex.printStackTrace();
 			}
 		}
@@ -44,6 +194,7 @@ public class MaincraftServer {
 		}
 	}
 
+
 	private void testQueue() {
 		Queue<Integer> queue = new LinkedList<>();
 
@@ -57,6 +208,7 @@ public class MaincraftServer {
 			System.out.println("Now serving ticket number: " + queue.poll());
 		}
 	}
+
 
 	private void testSorting() {
 		Set<Integer> numbers = new TreeSet<>(Arrays.asList(1, 5, 353, 4, 0));
@@ -85,6 +237,7 @@ public class MaincraftServer {
 		System.out.println("customSortedMap: " + customSortedMap + '\n');
 	}
 
+
 	private void testEntities() {
 		EntityRegistry entityRegistry = new EntityRegistry();
 
@@ -99,6 +252,7 @@ public class MaincraftServer {
 		System.out.println(entityRegistry.getEntities());
 	}
 
+
 	private void testBlocks() {
 		World world = new World();
 
@@ -111,6 +265,7 @@ public class MaincraftServer {
 		System.out.println(world.getBlocks());
 	}
 
+
 	private void testUnmodifiableList() {
 		List<Integer> list = new ArrayList<>();
 		list.add(1);
@@ -118,6 +273,7 @@ public class MaincraftServer {
 		list.add(1);
 		list = Collections.unmodifiableList(list);
 	}
+
 
 	private void testSet() {
 		Set<Integer> set = new HashSet<>();
@@ -130,6 +286,7 @@ public class MaincraftServer {
 		System.out.println(set);
 	}
 
+
 	private void testWorld() {
 		World world = new World();
 //		world.add(new Block("GRASS", 1, 1, 1));
@@ -139,10 +296,12 @@ public class MaincraftServer {
 		System.out.println(world.getBlocks());
 	}
 
+
 	private void testBlueprint() {
 		Hostile hostile = new Skeleton("Skel", 20);
 		System.out.println(hostile);
 	}
+
 
 	private void listen() {
 
@@ -167,6 +326,7 @@ public class MaincraftServer {
 		System.out.println("Good bye.");*/
 	}
 
+
 	private void testLoops() {
 		//for (int number : Arrays.asList(10, 1, 2, 3, 5))
 		//	System.out.println("Number is " + number);
@@ -182,6 +342,7 @@ public class MaincraftServer {
 
 		System.out.println("The end.");
 	}
+
 
 	private void testErrors() {
 		Monster monster = new Skeleton("Example monster", 30);
